52b52ab74e1eac798098fb2a297e2502e859e18f
Carlos Solorzano <csolorzanoflores@gmail.com>
2021-01-26 11:15:42 -0600

    Two numbers failing

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -4,7 +4,12 @@ namespace Kata
     {
         public int Add(string s = null)
         {
-            return 0;
+            if (string.IsNullOrEmpty(s))
+            {
+                return 0;                
+            }
+
+            return int.Parse(s);
         }
     }
 }
