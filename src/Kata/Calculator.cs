227a4eff20f95d056e125f0224ab823ae414c155
Carlos Solorzano <csolorzanoflores@gmail.com>
2021-01-26 11:32:23 -0600

    negative failing

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 
 namespace Kata
@@ -11,7 +12,19 @@ namespace Kata
                 return 0;                
             }
 
-            var numbers = s.Split(',', '\n').Select(int.Parse);
+            var delimiter = "";
+            var values = s;
+            if (s.Contains("//"))
+            {
+                var parts = s.Split('\n');
+                delimiter = parts[0].Replace("//", "");
+                values = parts[1];
+            }
+
+            var delimiters = new[] {",", "\n"};
+            if (!string.IsNullOrEmpty(delimiter))
+                delimiters = new[] {",", "\n", delimiter};
+            var numbers = values.Split(delimiters, StringSplitOptions.None).Select(int.Parse);
             return numbers.Sum();
         }
     }
