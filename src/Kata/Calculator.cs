cf267447bd9b18501653befbce4c70b2e4d1cb50
Carlos Solorzano <csolorzanoflores@gmail.com>
2021-01-27 11:44:39 -0600

    negatives failing

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -10,7 +10,15 @@ namespace Kata
             if(string.IsNullOrEmpty(s))
                 return 0;
 
-            var numbers = s.Split(',', '\n').Select(int.Parse);
+            var delimiters = new[] {",", "\n"};
+            var content = s;
+            if (s.StartsWith("//"))
+            {
+                var parts = s.Split("\n");
+                delimiters = new[] {parts[0].Replace("//", "")};
+                content = parts[1];
+            }
+            var numbers = content.Split(delimiters, StringSplitOptions.None).Select(int.Parse);
             return numbers.Sum();
         }
     }
