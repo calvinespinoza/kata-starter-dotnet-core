15e7b6bd905b350b4d78dcdd298313ec9fead972
Byron Sommardahl <byron@acklenavenue.com>
2021-02-02 13:51:29 -0600

    Resetting for the kata demo.

--- src/Kata.Spec/when_feeding_the_monkey.cs
+++ src/Kata.Spec/when_feeding_the_monkey.cs
@@ -16,77 +16,16 @@ namespace Kata.Spec
         It should_have_the_food_in_its_belly = () =>
             _systemUnderTest.Belly.Should().Contain("banana");
     }
-
-    public class when_user_input_is_empty
-    {
-        Establish _context = () => { _systemUnderTest = new Calculator(); };
-
-        Because of = () => { _result = _systemUnderTest.Add(); };
-
-        It should_return_zero = () => { _result.Should().Be(0); };
-        static Calculator _systemUnderTest;
-        static int _result;
-    }
-
-    public class with_user_input
-    {
-        Establish _context = () => { _systemUnderTest = new Calculator(); };
-
-        Because of = () => { _result = _systemUnderTest.Add("3"); };
-        
-        It  should_return_number  = () => { _result.Should().Be(3); };
-        static Calculator _systemUnderTest;
-        static int _result;
-    }
-
-    public class with_two_numbers
-    {
-        Establish _context = () => { _systemUnderTest = new Calculator(); };
-
-        Because of = () => { _result = _systemUnderTest.Add("1,2"); };
-        
-        It  should_return_number  = () => { _result.Should().Be(3); };
-        static Calculator _systemUnderTest;
-        static int _result;
-    }
-
-    public class when_adding_multiple_numbers
-    {
-        Establish _context = () =>
-        {
-            _systemUnderTest = new Calculator();
-        };
-
-        Because of = () => { _result = _systemUnderTest.Add("1,2,3"); };
-
-        It should_do_something = () => { _result.Should().Be(6); };
-        static Calculator _systemUnderTest;
-        static int _result;
-    }
-
-    public class when_using_a_custom_delimiter
-    {
-        Establish _context = () =>
-        {
-            
-            _systemUnderTest = new Calculator();
-        };
-
-        Because of = () =>
-        {
-            _result = _systemUnderTest.Add("//;\n1;2;3");
-        };
-
-        It should_return_the_sum_of_the_numners = () => { _result.Should().Be(6); };
-        static Calculator _systemUnderTest;
-        static int _result;
-    }
-    //    5. Given the user input is multiple numbers with new line and comma delimiters when calculating the sum then it should return the sum of all the numbers. (example "1\n2,3" should equal 6)
-    //    6. Given the user input is multiple numbers with a custom single-character delimiter when calculating the sum then it should return the sum of all the numbers. (example “//;\n1;2” should return 3)
-    //    7. Given the user input contains one negative number when calculating the sum then it should throw an exception "negatives not allowed: x" (where x is the negative number).
-    //    8. Given the user input contains multiple negative numbers mixed with positive numbers when calculating the sum then it should throw an exception "negatives not allowed: x, y, z" (where x, y, z are only the negative numbers). 
-    //    9. Given the user input contains numbers larger than 1000 when calculating the sum it should only sum the numbers less than 1001. (example 2 + 1001 = 2)
-    //    10. Given the user input is multiple numbers with a custom multi-character delimiter when calculating the sum then it should return the sum of all the numbers. (example: “//[***]\n1***2***3” should return 6)
-    //    11. Given the user input is multiple numbers with multiple custom delimiters when calculating the sum then it should return the sum of all the numbers. (example “//[*][%]\n1*2%3” should return 6)
-
+    
+    // Given the user input is empty when calculating the sum then it should return zero.
+    // Given the user input is one number when calculating the sum then it should return the same number. (example "3" should equal 3)
+    // Given the user input is two numbers when calculating the sum then it should return the sum of those numbers. (example "1,2" should equal 3)
+    // Given the user input is an unknown amount of numbers when calculating the sum then it should return the sum of all the numbers. (example "1,2,3" should equal 6)
+    // Given the user input is multiple numbers with new line and comma delimiters when calculating the sum then it should return the sum of all the numbers. (example "1\n2,3" should equal 6)
+    // Given the user input is multiple numbers with a custom single-character delimiter when calculating the sum then it should return the sum of all the numbers. (example “//;\n1;2” should return 3)
+    // Given the user input contains one negative number when calculating the sum then it should throw an exception "negatives not allowed: x" (where x is the negative number).
+    // Given the user input contains multiple negative numbers mixed with positive numbers when calculating the sum then it should throw an exception "negatives not allowed: x, y, z" (where x, y, z are only the negative numbers).
+    // Given the user input contains numbers larger than 1000 when calculating the sum it should only sum the numbers less than 1001. (example 2 + 1001 = 2)
+    // Given the user input is multiple numbers with a custom multi-character delimiter when calculating the sum then it should return the sum of all the numbers. (example: “//[]\n12***3” should return 6)
+    // Given the user input is multiple numbers with multiple custom delimiters when calculating the sum then it should return the sum of all the numbers. (example “//[][%]\n12%3” should return 6)
 }
